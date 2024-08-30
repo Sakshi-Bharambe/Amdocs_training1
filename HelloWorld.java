e8de5bb9a42f74cfc12d5b6810167098f7973f7c
Sakshi-Bharambe <97464248+Sakshi-Bharambe@users.noreply.github.com>
2024-08-30 14:40:18 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     static void method(){
-       System.out.println("Hello Java Method")
+       System.out.println("Hello Java Method");
      System.out.println("Hello Changed Java Method");
      }
     static void method2(){
